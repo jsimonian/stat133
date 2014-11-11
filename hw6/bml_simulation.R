38688aa477dedc84f158739b593110c4aebfd5e5
Joseph Simonian <jsimonian@berkeley.edu>
2014-11-11 01:18:42 -0800

    Simulation explainations
    Added some information for the graders on why I didn’t save stuff.

--- hw6/bml_simulation.R
+++ hw6/bml_simulation.R
@@ -9,6 +9,15 @@
 
 ### These all take a very long time to run.
 
+#### To be honest, I never found myself needing to save any data to the disk,
+#### But I figured I should show that I know how. So here's an example. I commented
+#### it out so that you wouldn't have random data being put onto your computer.
+
+#saveRDS(result.matrix, "1.RData")
+#res.1 <- load("1.RData")
+
+#### All that being said, here are the matriciesof data that I generated to get my results.
+
 result.matrix.1 <- matrix(replicate(10, sapply(c(1:100), function (x) bml.sim(10, 10, x/100)[[3]])), 100, 10)
 plot(c(1:100)/100, 10*apply(result.matrix.1, 1, sum), type = "l", xlab = "Density", ylab = "Percent of grids reaching lockup", main = "Density vs Chance of Gridlock")
 
@@ -17,4 +26,3 @@ plot(c(1:100)/100, apply(result.matrix.2, 1, mean, na.rm = T), type = "l", xlab 
 
 result.matrix.3 <- matrix(replicate(10, sapply(c(5:50), function (x) bml.sim(x, x, 0.5)[[3]])), 46, 10)
 plot(c(5:50), 10*apply(result.matrix.3, 1, sum), type = "l", xlab = "Matrix size", ylab = "Percent of grids reaching lockup", main = "Matrix Size vs Chance of Gridlock")
-
